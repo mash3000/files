31f987f92cfd303993169f219a7b26e029438ae5
mash3000 <mash3000@users.noreply.github.com>
2017-04-04 12:02:28 -0400

    Performed the operations where I wrote the variables a b and c to a binary file

--- writetext.cpp
+++ writetext.cpp
@@ -8,10 +8,16 @@ int main( )
    short int a = -6730;
    float b = 68.123; 
    char c = 'J';
- 	ofstream file;
-	file.open("abc.txt");
-	file <<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
-   	file.close();
- 
+	
+ 	ofstream file("abc.bin",ios::binary);
+	if(file){
+	file.write(reinterpret_cast<char*>(&a), sizeof(a));
+	file.write(reinterpret_cast<char*>(&b), sizeof(b));
+	file.write(reinterpret_cast<char*>(&c), sizeof(c));
+	} else
+     cout<<"Error opening abc.bin"<<endl;//print error before program quits
+
+ file.close();//optional in this case (destructor will close it automatically)
+
    return 0;
 }
