1c595be07e6777a57ca629622d91c52948e202bb
mash3000 <mash3000@users.noreply.github.com>
2017-04-04 11:06:28 -0400

    Did the first exercise and recorded the hex values for a b and c. Also outputted to the file abc.txt

--- writetext.cpp
+++ writetext.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
- 
+
 int main( )
 {
    //create some data
    short int a = -6730;
    float b = 68.123; 
    char c = 'J';
- 
-   cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
-
-   cout<<endl<<endl;
+ 	ofstream file;
+	file.open("abc.txt");
+	file <<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+   	file.close();
  
    return 0;
 }
